61caa95413835651b2e59f84cb7c1f248686645f
vikrant-honbute <vikranthonbute2004@gmail.com>
2026-07-08 18:34:24 +0530

    Bronze ingestion

---
+++ dags/flight_pipeline.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+from scripts.bronze_ingest import run_bronze_ingestion
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="flight_ops_medallion_pipeline",
+    default_args=default_args,
+    start_date=datetime(2026, 7, 5),
+    schedule_interval="*/30 * * * *",
+    catchup=False,
+) as dag:
+
+    bronze = PythonOperator(
+        task_id="bronze_ingest",
+        python_callable=run_bronze_ingestion,
+        
+    )
--- scripts/bronze_ingest.py
+++ scripts/bronze_ingest.py
@@ -4,3 +4,18 @@ from datetime import datetime
 from pathlib import Path
 
 URL = "https://opensky-network.org/api/states/all"
+
+def run_bronze_ingestion(**context):
+    response = requests.get(URL, timeout=10)
+    response.raise_for_status()  # Raise an error for bad responses
+
+    data = response.json()
+
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+
+    path = Path(f"/opt/airflow/data/bronze/flights_{timestamp}.json")
+
+    with open(path, "w") as f:
+        json.dump(data, f)
+
+    context['ti'].xcom_push(key='bronze_file', value=str(path))
